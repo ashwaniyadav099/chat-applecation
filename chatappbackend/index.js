5f3f79717b0988da5d706eca21ab5926e37cf6f4
ashwaniyadav099 <yadavashwani099@gmail.com>
2025-01-19 15:50:35 +0530

    simple chat app

--- chatappbackend/index.js
+++ chatappbackend/index.js
@@ -6,18 +6,26 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5174", // React app URL
+    origin: "http://localhost:5173", // React app URL
     methods: ["GET", "POST"]
   }
 });
 
 io.on('connection', (socket) => {
+  const customId = socket.handshake.query.customId;
+
+  // Optionally set the socket's ID to the custom ID
+  if (customId) {
+    socket.id = customId;
+  }
   console.log('A user connected:', socket.id);
 
   // Receive and broadcast messages
   socket.on('message', (data) => {
-    console.log('Message received:', data);
-    io.emit('message', data); // Broadcast message to all connected clients
+    io.emit('message', {
+      id: socket.id,
+      message: data
+    }); // Broadcast message to all connected clients
   });
 
   socket.on('disconnect', () => {
--- chatuser1/src/App.jsx
+++ chatuser1/src/App.jsx
@@ -1,16 +1,18 @@
-import  { useState, useEffect } from 'react';
+import React ,  { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
-
-const socket = io('http://localhost:8000'); // Connect to the Node.js server
+import "./App.css"
+import iamge from '../public/blank_profile.png'
+import { IoSend } from "react-icons/io5";
+const customId = "user" + Math.round(Math.random() * 10);
+const socket = io('http://localhost:8000' ,{
+  query: { customId }
+}); // Connect to the Node.js server
 
 function App() {
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
-
   useEffect(() => {
-    // Listen for incoming messages
     socket.on('message', (data) => {
-      console.log(data)
       setChat((prev) => [...prev, data]);
     });
 
@@ -18,30 +20,65 @@ function App() {
       socket.off('message');
     };
   }, []);
-
   const sendMessage = () => {
     if (message) {
       socket.emit('message', message); // Send a message to the server
       setMessage('');
     }
   };
-
   return (
-    <div style={{ padding: '20px' }}>
-    <h1>Socket.IO Chat</h1>
-    <div>
+  
+   <React.Fragment>
+    <div className="split left">
+      <div className="phone-screen">
+        <div className="header">
+           <div className='profile'>
+            <div className='imageCircle'><img src={iamge} alt='profile'/></div>
+            <div className='name'>John Doe</div>
+           </div>
+        </div>
+        <div className="chat_wrapper">
+        {
+          chat.map((msg, index) => {
+            if(msg.id === customId){
+             return  (<div key={index} className="My_msg">
+                   <div className='msg_wrap'>
+                      <p className="msg-text">{msg.message}</p>
+                      <span>{new Date().getTime}</span>
+                      </div>
+                 </div>)
+            }else{
+              return (<div key={index} className="Other_msg">
+                   <div className='msg_wrap'>
+
+                    <p className="msg-text">{msg.message}</p>
+                    <span>{new Date().getTime}</span>
+                    </div>
+              </div>)
+            }
+          })
+        }
+        </div>
+        <nav className="bottom-nav">
+          {/* <div>
       {chat.map((msg, index) => (
         <p key={index}>{msg}</p>
       ))}
-    </div>
+    </div> */}
     <input
       type="text"
       value={message}
       onChange={(e) => setMessage(e.target.value)}
       placeholder="Type a message..."
     />
-    <button onClick={sendMessage}>Send</button>
-  </div>
+    <button onClick={sendMessage}><span><IoSend color='#fff' size={22} /></span>    </button>
+
+        </nav>
+      </div>
+    </div>
+   
+    </React.Fragment>
+  
   )
 }
 
